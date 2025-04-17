0e7459a4a893a0d16721c5de542163d459abde87
ErfanBehrouzi <erfanbehrouzi2004@gmail.com>
2025-04-17 12:32:32 +0330

    clean the Program.cs

---
+++ Presentation/Configuration/BotConfigurationHelper.cs
@@ -0,0 +1,29 @@
+﻿using Telegram.Bot;
+
+namespace Presentation.Configuration;
+
+public static class BotConfigurationHelper
+{
+    public static TelegramBotClient CreateClient(CancellationToken cancellationToken)
+    {
+        var botToken = AppSettings.Root.Bot.Token;
+        if (string.IsNullOrEmpty(botToken))
+            throw new NullReferenceException("Telegram bot token environment variable is missing.");
+
+        return new TelegramBotClient(botToken, cancellationToken: cancellationToken);
+    }
+
+    public static async Task StopOnKeyStroke(TelegramBotClient bot, ConsoleKey consoleKey, CancellationTokenSource cts)
+    {
+        var me = await bot.GetMe();
+        Console.WriteLine($"@{me.Username} is running... \tPress '{consoleKey}' to terminate");
+
+        while (Console.ReadKey(true).Key != consoleKey)
+        {
+        }
+
+        await cts.CancelAsync(); // stop the bot
+
+        Console.WriteLine($"@{me.Username} terminated.");
+    }
+}
---
+++ Presentation/Extensions/TelegramBotClientExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Presentation.Controllers;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Presentation.Extensions;
+
+public static class TelegramBotClientExtensions
+{
+    public static void AddOnMessage(this TelegramBotClient bot)
+    {
+        var onMessageController = new OnMessageController(bot);
+        var onMessageMethods = OnMessageController.GetOnMessageMethods();
+
+        bot.OnMessage += OnMessage;
+        return;
+
+        async Task OnMessage(Message msg, UpdateType type)
+        {
+            onMessageController.InitialHandle(msg, type);
+            if (msg.Text is null) return;
+
+            var matchedMethod = onMessageMethods
+                .FirstOrDefault(x => x.Attribute!.Text.Equals(msg.Text, StringComparison.OrdinalIgnoreCase));
+            if (matchedMethod == default)
+            {
+                await onMessageController.HandleNoMatchingMessage(msg);
+                return;
+            }
+
+            await (Task)matchedMethod.Method.Invoke(onMessageController, [msg])!;
+        }
+    }
+}
